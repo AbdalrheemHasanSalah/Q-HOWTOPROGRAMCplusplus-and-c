59cc89b6cea1154e113b27302a3b39d60af4ef37
56165abd123 <abdsalahhasan2003@gmail.com>
2024-06-13 18:35:39 +0300

    Update Q2.18 HOWTOPROGRAM C++.cpp

--- Q2.18 HOWTOPROGRAM C++.cpp
+++ Q2.18 HOWTOPROGRAM C++.cpp
@@ -1,5 +1,10 @@
-// Q2.16 HOWTOPROGRAM C++.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// 2.18 (Comparing Values) Write a program that asks the user to enter the highest rainfall ever in
+//one season for a country, and the rainfall in the current year for that country, obtains the values from
+//the user, checks if the current rainfall exceed the highest rainfall and prints an appropriate message
+//on the screen. If the current rainfall is higher, it assigns that value as the highest rainfall ever. Use
+//only the single-selection form of the if statement .
+
+
 #include<iostream> 
 using namespace std;
 #include <stdio.h>
