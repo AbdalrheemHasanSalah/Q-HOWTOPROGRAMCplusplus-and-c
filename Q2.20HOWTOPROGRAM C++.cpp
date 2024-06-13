6190dbc004e62f63bec96c65af437d98e0e2de31
56165abd123 <abdsalahhasan2003@gmail.com>
2024-06-13 18:37:26 +0300

    Update Q2.20HOWTOPROGRAM C++.cpp

--- Q2.20HOWTOPROGRAM C++.cpp
+++ Q2.20HOWTOPROGRAM C++.cpp
@@ -1,5 +1,10 @@
 // Q2.20 HOWTOPROGRAM C++.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+
+//2.20 (Converting from seconds to hours, minutes and seconds) Write a program that asks the
+//user to enter the total time elapsed, in seconds, since an event and converts the time to hours,
+//minutes and seconds. The time should be displayed as hours:minutes:seconds. [Hint: Use theremainder operator]
+
+
 #include<iostream> 
 using namespace std;
 #include <stdio.h>
