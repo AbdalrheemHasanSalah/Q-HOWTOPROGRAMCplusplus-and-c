8bf1117739973992a447c3f472989dcf7589be4f
56165abd123 <abdsalahhasan2003@gmail.com>
2024-06-13 20:36:53 +0300

    Add files via upload

---
+++ Q2.25HOWTOPROGRAM C++.cpp
@@ -0,0 +1,34 @@
+// Q2.25 HOWTOPROGRAM C++.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//2.25 Print your initials in block letters down the page. Construct each block letter out of the letter it represents, 
+// as shown on the top of the next page:
+
+#include<iostream> 
+using namespace std;
+#include <stdio.h>
+#include <math.h>
+#include <string>
+
+
+int main()
+{
+	printf("    PPPPPPPPP\n");
+	for(int i=0;i<3;i++)
+	   printf("\tP   P\n");
+	   printf("\t P P\n\n");
+	
+	   printf("     JJ\n");
+	   printf("   J\n");
+	   printf(" J\n");
+	   printf("  J\n");
+	   printf("   J\n");
+	   printf("     JJJJJJJ\n\n");
+
+	   printf(" DDDDDDDDD\n"); 
+	   printf(" D       D\n");
+	   printf(" D       D\n");
+	   printf("  D     D\n");
+	   printf("   DDDDD ");
+
+}
+
+
