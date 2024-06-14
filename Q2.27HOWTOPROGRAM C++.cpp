202eb83137281e20504dbf49e8d15f9b0947bbae
56165abd123 <abdsalahhasan2003@gmail.com>
2024-06-14 09:50:56 +0300

    Add files via upload

---
+++ Q2.27HOWTOPROGRAM C++.cpp
@@ -0,0 +1,28 @@
+// Q2.27 HOWTOPROGRAM C++.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//2.27  (Checkerboard Pattern of Asterisks)
+
+
+#include<iostream> 
+using namespace std;
+#include <stdio.h>
+#include <math.h>
+#include <string>
+
+
+int main()
+{
+	int x;
+	cout << "input line number ";
+	cin >> x;
+	for (int i = 0; i <= x; i++)
+	{
+		printf("* * * * * * * *\n");
+		printf("   * * * * * * * *\n");
+	}
+	
+
+}
+
+
+
+
