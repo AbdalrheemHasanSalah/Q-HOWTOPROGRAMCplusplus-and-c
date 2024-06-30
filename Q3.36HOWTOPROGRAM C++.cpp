731a6bea1fa4d21f08b4fb359e4f4f8e42107d59
56165abd123 <abdsalahhasan2003@gmail.com>
2024-06-30 14:02:43 +0300

    Add files via upload

---
+++ Q3.36HOWTOPROGRAM C++.cpp
@@ -0,0 +1,45 @@
+// Q3.36 HOWTOPROGRAM C++.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//3.36 (Armstrong Numbers) Armstrong numbers are numbers that are equal to the sum of their digits raised to power of the number of digits in them.The number 153, for example, equals 13 + 53 + 33 .
+//Thus it is an Armstrong number.Write a program to display all three - digit Armstrong numbers
+
+
+
+#include<iostream> 
+#include<stdio.h>
+#include<math.h>
+using namespace std;
+
+int main()
+{ 
+	int Armstrong_number;
+	cin >> Armstrong_number;
+	int temp = Armstrong_number;
+	int count = 0;
+	int sum = 0;
+	
+	while (temp != 0)
+	{
+		temp=temp / 10;
+		count++;
+	}
+	int i = 0;
+	int* arr = new int(count);
+	temp = Armstrong_number;
+	while (temp != 0)
+	{
+		*(arr + i) = temp % 10;
+		sum+=pow(*(arr + i), count);
+		temp = temp / 10;
+
+
+	}
+	if (sum == Armstrong_number) {
+		cout << sum << " is armstrong number .";
+	}
+	else 
+	cout<< sum <<" is not armstrong number .";
+}
+
+
+
+
