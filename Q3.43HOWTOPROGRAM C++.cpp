b6dfeb905053312058101ee5f31bbc66ae6cb588
56165abd123 <abdsalahhasan2003@gmail.com>
2024-07-01 12:15:37 +0300

    Add files via upload

---
+++ Q3.43HOWTOPROGRAM C++.cpp
@@ -0,0 +1,28 @@
+// Q3.43 how to program c++.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//3.43 (Sides of a Triangle) Write a program that reads three nonzero integer values and determines 
+//and prints whether they could represent the sides of a triangle.
+
+#include <iostream>
+#include<cstdio>
+#include<stdio.h>
+
+int main()
+{
+	int a, b, c;
+	std::cout << "the first side is :";
+	std::cin >> a;
+	std::cout << "\nthe seconed side is :";
+	std::cin >> b;
+	std::cout << "\nthe third side is :";
+	std::cin >> c;
+
+	if (a + b < c)printf("they could not be side triangle");
+	else if (a +c < b)printf("they could not be side triangle");
+	else if (b + c < a)printf("they could not be side triangle");
+	else
+   printf("they could be side triangle");
+
+}
+
+
+
